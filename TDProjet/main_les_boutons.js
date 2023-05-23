ee3a88cfea3db62a2e2f469bf82cf38b7ab1ebf3
GoldenSpace6 <74157253+GoldenSpace6@users.noreply.github.com>
2023-05-23 16:59:05 +0200

    Delete main_les_boutons.js

--- TDProjet/main_les_boutons.js
+++
@@ -1,7 +0,0 @@
-const link = document.querySelectorAll('.bouton_jeune');
-links.forEach(function(link){ 
-    link.addEventListener('click', function(event){
-        event.preventDefault();
-        link.classList.add('clicked');
-    });
-});
