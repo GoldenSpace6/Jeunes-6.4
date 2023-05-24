939514964d8b648473af69d7d5fb3ddb8897566f
GoldenSpace6 <74157253+GoldenSpace6@users.noreply.github.com>
2023-05-24 18:23:40 +0200

    Update connexion.php

--- TDProjet/connexion.php
+++ TDProjet/connexion.php
@@ -56,9 +56,9 @@
     <div class="haut_de_page_vide"></div>
     <div class="bas_de_page">  
         <div class="les_modules">
-                <li> <a class="bouton_jeune" href="jeune.html">JEUNE</a> </li>
-                <li> <a class="bouton_referent" href="referent.html">REFERENT</a> </li>
-                <li> <a class="bouton_consultant" href="consultant.html">CONSULTANT</a>  </li> 
+                <li> <a class="bouton_jeune" href="jeune.php">JEUNE</a> </li>
+                <li> <a class="bouton_referent" href="referent.php">REFERENT</a> </li>
+                <li> <a class="bouton_consultant" href="consultant.php">CONSULTANT</a>  </li> 
                 <li> <a class="bouton_partenaire" href="partenaire.html">PARTENAIRES</a> </li>
         </div>
         
