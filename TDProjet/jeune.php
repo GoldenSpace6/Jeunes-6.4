963bc10d91026df52328c96bf7bbee82b42c333d
GoldenSpace6 <74157253+GoldenSpace6@users.noreply.github.com>
2023-05-30 17:45:01 +0200

    Update jeune.php

--- TDProjet/jeune.php
+++ TDProjet/jeune.php
@@ -1,7 +1,8 @@
 <html lang="fr">
 <head>
 
-    <link rel="stylesheet" href="main.css" type="text/css">
+    <link rel="stylesheet" href="CSS/main.css" type="text/css">
+    <link rel="stylesheet" href="CSS/formulaire.css" type="text/css">
     <title>Jeunes 6.4 - Jeune</title>
     <?php
     use PHPMailer\PHPMailer\PHPMailer;
