db2041c8a16c719f3cfd022ae16761048b655b61
GoldenSpace6 <74157253+GoldenSpace6@users.noreply.github.com>
2023-06-11 17:25:58 +0200

    +consultant.php

--- TDProjet/consultant.php
+++ TDProjet/consultant.php
@@ -10,7 +10,28 @@
 
     <?php 
 	require("script/phpfonction.php");
+    $lien = previousPage();
+
+    //Remplissage automatique des champs 
+    if(isset($_GET["id"])){
+            
+        //Recupère l'id dans l'url
+        $urlid = $_GET["id"];
+        
+        /*Recupère les données de la demande de référence*/
+        $d_id = getrefid($d_data,$urlid);
+
+        if($d_id != -1) {
+            $demande = $d_data[$d_id];
+            $referent = $demande["referent"];
+            $jeune = $demande["jeune"];
+        } else {
+            $lien = previousPage();
+        }
+    } else {
         $lien = previousPage();
+    }
+
     ?>
 
 </head>
@@ -47,47 +68,37 @@
             <img src="image/6.4_Bleu.png" >
         </div>
         <div class="carre_consultant">
-                <div class="carre_jeune">
-                    <form action="profil.php" method="POST" class="texte_carre_jeune couleur_jeune carre_formulaire">
-                        <p class="couleur_jeune nom"> JEUNE </p>
-                        <!--<//?php echo $err?><br>-->
-                        <label for="nom">Nom:</label>
-                        <input type="text" name="nom" id="nom" value="<?php echo $_SESSION["info"]["nom"];?>" disabled><br>
-                        <label for="prenom">Prénom:</label>
-                        <input type="text" name="prenom" id="prenom" value="<?php echo $_SESSION["info"]["prenom"];?>" disabled><br>
-                        <label for="date">Date de naissance:</label>
-                        <input type="Date" name="date" id="date" value="<?php echo $_SESSION["info"]["date"];?>" disabled><br>
-                        <label for="e-mail">E-mail:</label>
-                        <input type="e-mail" name="e-mail" id="e-mail" value="<?php echo $_SESSION["info"]["mail"];?>" disabled><br>
-                        <label for="reseau">Réseau social :</label>
-                        <input type="text" name="reseau" id="reseau_social" value="<?php echo $jeune["nom"]?>"><br><br><br>
-                        <label for="presentation">Mon engagement :</label>
-                        <input type="text" name="engagement" id="engagement" value="<?php echo $demande["engagement"]?>"><br>
-                        <label for="duree">Durée :</label>
-                        <input type="text" name="duree" id="duree" value="<?php echo $demande["duree"]?>"><br>
-                    </form>
-                </div>
-                <div class="carre_referent">
-                    <form action="profil.php" method="POST" class="texte_carre_referent couleur_referent carre_formulaire">
-                        <p class="couleur_referent nom"> REFERENT </p>
-                        <!--<//?php echo $err?><br>-->
-                        <label for="nom">Nom:</label>
-                        <input type="text" name="nom" id="nom" value="<?php echo $_SESSION["info"]["nom"];?>" disabled><br>
-                        <label for="prenom">Prénom:</label>
-                        <input type="text" name="prenom" id="prenom" value="<?php echo $_SESSION["info"]["prenom"];?>" disabled><br>
-                        <label for="date">Date de naissance:</label>
-                        <input type="Date" name="date" id="date" value="<?php echo $_SESSION["info"]["date"];?>" disabled><br>
-                        <label for="e-mail">E-mail:</label>
-                        <input type="e-mail" name="e-mail" id="e-mail" value="<?php echo $_SESSION["info"]["mail"];?>" disabled><br>
-                        <label for="reseau">Réseau social :</label>
-                        <input type="text" name="reseau" id="reseau_social" value="<?php echo $jeune["nom"]?>"><br><br><br>
-                        <label for="presentation">Présentation :</label>
-                        <input type="text" name="presentation" id="presentation" value="<?php echo $demande["engagement"]?>"><br>
-                        <label for="duree">Durée :</label>
-                        <input type="text" name="duree" id="duree" value="<?php echo $demande["duree"]?>"><br>
-                    </form>
+            <div class="carre_jeune">
+                <div action="profil.php" method="POST" class="texte_carre_jeune couleur_jeune carre_formulaire">
+                    <p class="couleur_jeune nom"> JEUNE </p>
+                    <label for="nom">Nom :</label>
+                    <input type="text" name="j_nom" id="j_nom" value="<?php echo $jeune["nom"]?>" disabled><br>
+                    <label for="prenom">Prénom :</label>
+                    <input type="text" name="j_prenom" id="j_prenom" value="<?php echo $jeune["prenom"]?>" disabled><br>
+                    <label for="mail">Mail :</label>
+                    <input type="e-mail" name="j_e-mail" id="j_e-mail" value="<?php echo $jeune["mail"]?>" disabled><br>
+                    <label for="date">Date de naissance :</label>
+                    <input type="date" name="date" id="date_naissance" value="<?php echo $jeune["date"]?>" disabled><br>
                 </div>
             </div>
+            <div class="carre_referent">
+                <div action="profil.php" method="POST" class="texte_carre_referent couleur_referent carre_formulaire">
+                    <p class="couleur_referent nom"> REFERENT </p>
+
+                    <label for="nom">Nom :</label>
+                    <input type="text" name="nom" id="nom" value="<?php echo $referent["nom"]?>"><br>
+                    <label for="prenom">Prénom :</label>
+                    <input type="text" name="prenom" id="prenom" value="<?php echo $referent["prenom"]?>"><br>
+                    <label for="mail">Mail :</label>
+                    <input type="e-mail" name="e-mail" id="e-mail" value="<?php echo $referent["mail"]?>"><br>
+                    <br><br><br>
+                    <label for="presentation">Présentation :</label>
+                    <input type="text" name="presentation" id="presentation" value="<?php echo $demande["engagement"]?>"><br>
+                    <label for="duree">Durée :</label>
+                    <input type="text" name="duree" id="duree" value="<?php echo $demande["duree"]?>"><br>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
